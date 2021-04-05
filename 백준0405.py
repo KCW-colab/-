5f072eb81686c412ff25a806d24b09b67bf4d395
KCW-colab <68315955+KCW-colab@users.noreply.github.com>
2021-04-05 23:25:42 +0900

    Update 백준0405.py

--- 백준0405.py
+++ 백준0405.py
@@ -34,19 +34,3 @@ while True :
     right = mid - 1
   elif mid ** 2 < n:
     left = mid + 1
-
-# 이분탐색
-n = int(input())
-low = 1
-high = n
-
-while 1:
-    mid = (low + high) // 2
-    if mid ** 2 == n:
-        print(mid)
-        break
-    elif mid ** 2 > n:
-        high = mid - 1
-    elif mid ** 2 < n:
-        low = mid + 1
-
